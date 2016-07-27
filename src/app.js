57cdad0c37363b8f00ff1cc166e74b1da60645df
nishanth <nishgowda3@gmail.com>
2016-07-27 19:12:25 +0100

    application

--- src/app.js
+++ src/app.js
@@ -18,6 +18,10 @@ app.get('/', function(req,res){
   res.render('index');
 });
 
+app.get('/about', function(req,res){
+  res.render('about');
+});
+
 app.listen(3000,function(){
   console.log("The server is running at port 3000");
 });
